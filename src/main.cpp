5558ce6351d7976b4756f4399cdf6013ce5a2b7d
mazuzel <mazuzel@protonmail.com>
2024-06-23 14:43:12 +0200

    add windows titles

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@ int main() {
 	settings.resizable = false;
 	settings.windowMode = OF_WINDOW;
 	settings.monitor = 1;
+	settings.title = "Tonton Media Player - DAW";
 	shared_ptr<ofAppBaseWindow> dawWindow = ofCreateWindow(settings);
 
 	shared_ptr<ofApp> mainApp(new ofApp);
@@ -33,6 +34,7 @@ int main() {
 		settings.resizable = true;
 		settings.windowMode = OF_WINDOW;
 		settings.monitor = 0;
+		settings.title = "Tonton Media Player - Video Output";
 		// uncomment next line to share main's OpenGL resources with gui
 		settings.shareContextWith = dawWindow;
 		shared_ptr<ofAppBaseWindow> mappingWindow = ofCreateWindow(settings);
